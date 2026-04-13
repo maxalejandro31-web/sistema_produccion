26348972d62406084dcfc967f953ca797b1ec5fc
Maximo Trejo <max_alejandro31@icloud.com>
2026-04-13 13:08:49 -0600

    fix import exportar_excel dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import inicio, exportar_excel, login_view, logout_view
+from .views import inicio, login_view, logout_view
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path('exportar/', exportar_excel, name='exportar_excel'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
 ]
